8ae51ee73530a4238f13eef60e5561c759dcf7de
DmitriiGienko <cuanik@mail.ru>
2023-07-04 13:55:26 +0300

    Начало! ;)

---
+++ src/main/java/ru/skypro/coursework/easyauction/EasyAuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.coursework.easyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/coursework/easyauction/EasyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.coursework.easyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
